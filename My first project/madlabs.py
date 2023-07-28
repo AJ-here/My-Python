86177169c9f2001178fbb17f28b7aa548fa96fc9
Ajay <ajaynath158@gmail.com>
2023-07-28 20:34:17 +0530

    new file:   My first project/madlabs.py

---
+++ My first project/madlabs.py
@@ -0,0 +1,8 @@
+# Sample coding for concatenating the string 
+
+adj =input("adjective:")
+verb1= input ("Enter verb1:")
+verb2=input ("Enter verb2:")
+madlab= f"Computer is so {adj}! This is so exciting because \
+I love to {verb1} and we need to do {verb2}"
+print(madlab)
